a8c571fb67e0ad38a89d39b33e6c50d3ce9ce6a1
Matthew <matthew.beadman@gmail.com>
2021-01-08 16:04:34 +0000

    challengefeed creates an image from firestore

--- components/main/GroupScreens/ChallengeFeed.js
+++ components/main/GroupScreens/ChallengeFeed.js
@@ -1,37 +1,77 @@
 import React, { useEffect, useState } from "react";
-import { View, FlatList, Image } from "react-native";
+import { View, FlatList, Image, StyleSheet } from "react-native";
 import * as firebase from "firebase";
+import { Text } from "react-native";
 require("firebase/firestore");
 
 export default function ChallengeFeed({ navigation }) {
   const [submissions, setSubmissions] = useState([]);
-
+  //refactor to access specific paths
   useEffect(() => {
     firebase
       .firestore()
-      .collection("submissions")
-      .doc("3BSsarzldBgVkMXa4UkbpDWKkzx1")
-      .collection("userPosts")
-      .doc("kjfa19gPACsKWSQ67zTv")
+      .collection("groups")
+      .doc("0.tnaw7g00i")
+      .collection("challenges")
+      .doc("1")
+      .collection("uploads")
       .get()
       .then((snapshot) => {
-        console.log(snapshot.data());
+        let submissions = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          const id = doc.id;
+          return { id, ...data };
+        });
+        setSubmissions(submissions);
       });
-  });
+  }, []);
+  console.log(submissions);
+  // const renderItem = ({ item }) => {
+  //   <View style={{ flex: 1 }}>
+  //     <Text>{item.url}</Text>
+  //     <Text>Challenge</Text>
+  //     <Image
+  //       style={{
+  //         flex: 1,
+  //         aspectRatio: 1 / 1,
+  //       }}
+  //       resizeMode="center"
+  //       source={{ uri: item.url }}
+  //     />
+  //   </View>;
+  // };
 
-  const renderItem = ({ item }) => {
-    <View>
-      <Image
-        style={{
-          width: 50,
-          height: 50,
-        }}
-      />
-    </View>;
-  };
   return (
+    // <View>
+    //   <Image
+
+    //     source={{
+    //       uri:
+    //         "https://firebasestorage.googleapis.com/v0/b/activity-club-3dfcf.appspot.com/o/Hedgehog.jpg?alt=media&token=c5534e2a-922d-4e39-95b0-89b566902853",
+    //     }}
+    //   />
+    // </View>
     <View>
-      <FlatList numColumns={1} data={submissions} renderItem={renderItem} />
+      <FlatList
+        numColumns={1}
+        horizontal={false}
+        data={submissions}
+        renderItem={({ item }) => (
+          <View style={{ flex: 1 }}>
+            {/* <Text>{item.url}</Text>
+            <Text>Challenge</Text> */}
+            <Image
+              style={{
+                width: "auto",
+                height: 500,
+              }}
+              source={{
+                uri: item.url,
+              }}
+            />
+          </View>
+        )}
+      />
     </View>
   );
 }
