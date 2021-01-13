1dbf935d7faab4e7c87bb3d4839c39109378313d
Andy Patterson <andrewjp1992@gmail.com>
2021-01-13 10:20:24 +0000

    creates required fields on createGorup screen

--- components/main/Activity.js
+++ components/main/Activity.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, Button, StyleSheet } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchUser, selectUser } from "./redux/reducers/userSlice";
@@ -6,9 +6,11 @@ import { selectGroupsJoined } from "./redux/reducers/groupsSlice";
 import store from "../main/redux/store";
 
 const Activity = () => {
+  const getStarted = "hello new user, get started <Button/>";
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
   const groupsJoined = useSelector(selectGroupsJoined);
+  const [gettingStarted, removeGettingStarted] = useState(getStarted);
 
   const handlePress = async () => {
     console.log(fontFamily, "store before dispatch", store.getState());
@@ -17,7 +19,7 @@ const Activity = () => {
   return (
     <View style={styles.container}>
       <Text>Activity</Text>
-
+      {!!gettingStarted && <View>{gettingStarted}</View>}
       <Button title="button" onPress={() => handlePress()}>
         Click for users
       </Button>
